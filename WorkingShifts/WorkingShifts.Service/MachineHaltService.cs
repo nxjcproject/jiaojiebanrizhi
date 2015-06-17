dbb815d7cd59b88400dde7b68cba6a9dd1bbd718
decrula <decrula@gmail.com>
2015-06-17 08:50:37 +0800

    修复BUG：当时间班结束时间为24：00时，SQL溢出的问题。

--- WorkingShifts/WorkingShifts.Service/MachineHaltService.cs
+++ WorkingShifts/WorkingShifts.Service/MachineHaltService.cs
@@ -51,8 +51,8 @@ namespace WorkingShifts.Service
                                           FROM [shift_MachineHaltLog] AS [A], [system_Organization] AS [B]
                                          WHERE [A].[OrganizationID] = [B].[OrganizationID] AND 
                                                [B].[LevelCode] LIKE (SELECT [LevelCode] FROM [system_Organization] WHERE [OrganizationID] = @organizationId) + '%' AND 
-                                               [A].[HaltTime] >= @startTime AND
-                                               [A].[HaltTime] <  @endTime";
+                                               CONVERT(CHAR(19), [A].[HaltTime], 20) >= @startTime AND
+                                               CONVERT(CHAR(19), [A].[HaltTime], 20) <  @endTime";
 
                 command.Parameters.Add(new SqlParameter("organizationId", organizationId));
                 command.Parameters.Add(new SqlParameter("startTime", startTime));
